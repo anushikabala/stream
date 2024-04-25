9e9ecb629aca1fab47e87df34ff2e94095bb0575
anushikabala <anushikabala@gmail.com>
2024-04-25 15:19:54 +0530

    back

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,36 @@ import streamlit as st
 # Define your flowchart here
 flowchart = {
     "Start": {
-        "question": "Welcome! Would you like to start?",
-        "options": ["Yes", "No"],
-        "next": {"Yes": "Option 1", "No": "End"}
+        "question": "Please select the issue ....",
+        "options": [
+            
+            "Center Holes",
+            "Missing lenses", 
+            "Edge defect",
+            "Center Unknown",
+            "Others"
+            ],
+        "next": {"Missing lenses": "Option 1", "Center Holes": "End","Edge defect": "End","Center Unknown": "End","Others": "End"}
     },
     "Option 1": {
-        "question": "Option 1: Do you want more information?",
+        "question": "Are Missing Lens more than 5% ?",
         "options": ["Yes", "No"],
         "next": {"Yes": "Option 2", "No": "End"}
     },
     "Option 2": {
-        "question": "Option 2: Are you sure?",
-        "options": ["Yes", "No"],
-        "next": {"Yes": "End", "No": "Option 1"}
+        "question": "Do the need full : 1. TOL stable no gaps. 2. Total Tower time < 70 %  ",
+        "options": ["Done", "Back"],
+        "next": {"Done": "Opt 3", "Back": "Option 1"}
+    },
+    "Opt 3": {
+        "question": "Now Audit images from ALI and estimate the amount of lenses present in the sample.",
+        "options": ["Done", "Back"],
+        "next": {"Done": "Opt 4", "Back": "Option 2"}
+    },
+    "Opt 4": {
+        "question": "Are there inverted/ off center or folded lense",
+        "options": ["Yes", "No","Back",],
+        "next": {"Yes": "Opt 3", "No": "End","Back": "Opt 3"}
     },
     "End": {
         "question": "Thank you for using the chatbot!",
@@ -25,23 +42,13 @@ flowchart = {
 
 def chatbot():
     current_state = "Start"
-    previous_state = None
     
     while current_state != "End":
         question = flowchart[current_state]["question"]
         options = flowchart[current_state]["options"]
         
-        if current_state != "Start":
-            selected_option = st.radio(question, options)
-        else:
-            selected_option = st.selectbox(question, options)
+        selected_option = st.selectbox(question, options)
         
-        if selected_option == "No" and previous_state:
-            current_state = previous_state
-            previous_state = None
-            continue
-        
-        previous_state = current_state
         current_state = flowchart[current_state]["next"][selected_option]
 
 def main():
