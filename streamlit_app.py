5bb89f754edad08d17c02960ce8ecebdccb6e124
anushikabala <anushikabala@gmail.com>
2024-04-25 15:33:06 +0530

    endd

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,22 +17,22 @@ flowchart = {
     "Option 1": {
         "question": "Are Missing Lens more than 5% ?",
         "options": ["Select","Yes", "No"],
-        "next": {"Yes": "Option 2", "No": "End"}
+        "next": {"Select":"End","Yes": "Option 2", "No": "End"}
     },
     "Option 2": {
         "question": "Do the need full : 1. TOL stable no gaps. 2. Total Tower time < 70 %  ",
         "options": ["Select","Done", "Back"],
-        "next": {"Done": "Opt 3", "Back": "Option 1"}
+        "next": {"Select":"End","Done": "Opt 3", "Back": "Option 1"}
     },
     "Opt 3": {
         "question": "Now Audit images from ALI and estimate the amount of lenses present in the sample.",
         "options": ["Select","Done", "Back"],
-        "next": {"Done": "Opt 4", "Back": "Option 2"}
+        "next": {"Select":"End","Done": "Opt 4", "Back": "Option 2"}
     },
     "Opt 4": {
         "question": "Are there inverted/ off center or folded lense",
         "options": ["Select","Yes", "No","Back",],
-        "next": {"Yes": "Opt 3", "No": "End","Back": "Opt 3"}
+        "next": {"Select":"End","Yes": "Opt 3", "No": "End","Back": "Opt 3"}
     },
     "End": {
         "question": "Thank you for using the chatbot!",
@@ -48,12 +48,9 @@ def chatbot():
         options = flowchart[current_state]["options"]
         
         selected_option = st.selectbox(question, options)
+        
+        current_state = flowchart[current_state]["next"][selected_option]
 
-        if selected_option != "Select":  # Proceed only if a valid option is selected
-            try:
-                current_state = flowchart[current_state]["next"][selected_option]
-            except KeyError:
-                pass
 def main():
     st.title("Chatbot Demo")
     st.write("This is a simple chatbot demo.")
